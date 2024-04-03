09c75fe32e413849d74dcb1a16d36a371169458d
deivibarra <deivibarra@yahoo.com>
2024-04-03 07:22:52 -0500

    Adicionando un nuevo EnumModulosWeb

---
+++ src/Enum/EnumModulosWeb.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace softdin\servicio\Enum;
+
+use ReflectionClass;
+use Illuminate\Support\Collection;
+
+class EnumModulosWeb
+{
+    public const SOLICITUDES = 1;
+    public const INCAPACIDAD = 2;
+    public const TIEMPO = 3;
+
+    private static $descriptions = [
+        ['id' => self::SOLICITUDES, 'code' => 'SOLICITUDES', 'description' => 'Solicitud de Permiso, Licencias, Vacaciones'],
+        ['id' => self::INCAPACIDAD, 'code' => 'INCAPACIDAD', 'description' => 'Registro de Incapacidad'],
+        ['id' => self::TIEMPO, 'code' => 'TIEMPO', 'description' => 'Registro de Tiempo Suplementario'],
+    ];
+
+
+    public static function getCollection()
+    {
+        return collect(self::$descriptions);
+    }
+
+    public static function getById($id)
+    {
+        return self::getCollection()->firstWhere('id', $id) ?? null;
+    }
+
+    public static function getAll()
+    {
+        return self::$descriptions;
+    }
+
+    public static function getByDescription($description)
+    {
+        return self::getCollection()->firstWhere('description', $description) ?? null;
+    }
+
+}
