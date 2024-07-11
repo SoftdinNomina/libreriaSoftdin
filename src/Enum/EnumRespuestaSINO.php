659bb3fcf191e68e4820ac0db5170998b7fcbb2d
deivibarra <deivibarra@yahoo.com>
2024-07-11 07:42:08 -0500

    Actualizando EnumRespuestaSINO

--- src/Enum/EnumRespuestaSINO.php
+++ src/Enum/EnumRespuestaSINO.php
@@ -30,10 +30,36 @@ class EnumRespuestaSINO
     {
         return self::$descriptions;
     }
-
+ 
     public static function getByDescription($description)
     {
         return self::getCollection()->firstWhere('description', $description) ?? null;
+    }
+
+    private static $colorMapping = [
+        'success' => self::SI,
+        'danger' => self::NO, // Default color for unknown states
+    ];
+
+    public static function getColors($campo): array
+    {
+        $colorArray = [];
+
+        foreach (self::$colorMapping as $color => $description) {
+            $descriptionEntry = array_filter(self::$descriptions, fn($item) => $item['id'] === $description);
+            if (!empty($descriptionEntry)) {
+                $colorArray[$color] = array_shift($descriptionEntry)[$campo];
+            } else {
+                $colorArray[$color] = null; // Manejar el caso en que el campo no exista
+            }
+        }
+        return $colorArray;
+    }
+
+    public static function getColorName($campo, $valor)
+    {
+        $colors = self::getColors($campo);
+        return array_search($valor, $colors);
     }
 
 }
