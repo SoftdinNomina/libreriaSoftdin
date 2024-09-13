b3e6b196f52574e0aec4f3cd87360ce69eb5eb49
deivibarra <deivibarra@yahoo.com>
2024-09-13 07:54:33 -0500

    Actulalizando el modulo de EnumModelosNovedades

--- src/Enum/EnumModulosNovedades.php
+++ src/Enum/EnumModulosNovedades.php
@@ -24,21 +24,21 @@ class EnumModulosNovedades
     const PRESTAMOS = 15;
 
     private static $descriptions = [
-        ['id' => self::APORTE_VOLUNTARIO_SEGURIDAD_SOCIAL, 'code' => 'APORTE_VOLUNTARIO_SEGURIDAD_SOCIAL', 'description' => "Aporte Voluntario Seguridad Social"],
-        ['id' => self::BENEFICIO_PERSONAL, 'code' => 'BENEFICIO_PERSONAL', 'description' => "Beneficio Personal"],
-        ['id' => self::EMBARGOS, 'code' => 'EMBARGOS', 'description' => "Embargos"],
-        ['id' => self::FONDOS_AHORRO_REGISTROS, 'code' => 'FONDOS_AHORRO_REGISTROS', 'description' => "Fondos Ahorro Registros"],
-        ['id' => self::FONDOS_AHORRO_PAGO, 'code' => 'FONDOS_AHORRO_PAGO', 'description' => "Fondos Ahorro Pago"],
-        ['id' => self::HORAS_EXTRAS, 'code' => 'HORAS_EXTRAS', 'description' => "Horas Extras"],
-        ['id' => self::INGRESOS_DESCUENTOS_CONSECUTIVOS, 'code' => 'INGRESOS_DESCUENTOS_CONSECUTIVOS', 'description' => "Ingresos Descuentos Consecutivos"],
-        ['id' => self::NOVEDADES_CARGOS, 'code' => 'NOVEDADES_CARGOS', 'description' => "Novedades Cargos"],
-        ['id' => self::NOVEDADES_SEGURIDAD_SOCIAL, 'code' => 'NOVEDADES_SEGURIDAD_SOCIAL', 'description' => "Novedades Seguridad Social"],
-        ['id' => self::NOVEDADES_RETE_FUENTE, 'code' => 'NOVEDADES_RETE_FUENTE', 'description' => "Novedades Rete Fuente"],
-        ['id' => self::OTROS_INGRESOS_DESCUENTOS, 'code' => 'OTROS_INGRESOS_DESCUENTOS', 'description' => "Otros Ingresos Descuentos"],
-        ['id' => self::PAGOS_EXTERNOS, 'code' => 'PAGOS_EXTERNOS', 'description' => "Pagos Externos"],
-        ['id' => self::PAGO_LABOR, 'code' => 'PAGO_LABOR', 'description' => "Pago Labor"],
-        ['id' => self::PAGO_PRODUCCION, 'code' => 'PAGO_PRODUCCION', 'description' => "Pago Producción"],
-        ['id' => self::PRESTAMOS, 'code' => 'PRESTAMOS', 'description' => "Préstamos"],
+        ['id' => self::APORTE_VOLUNTARIO_SEGURIDAD_SOCIAL, 'code' => 6, 'description' => "Aporte Voluntario Seguridad Social"],
+        ['id' => self::BENEFICIO_PERSONAL, 'code' => 104, 'description' => "Beneficio Personal"],
+        ['id' => self::EMBARGOS, 'code' => 36, 'description' => "Embargos"],
+        ['id' => self::FONDOS_AHORRO_REGISTROS, 'code' => 42, 'description' => "Fondos Ahorro Registros"],
+        ['id' => self::FONDOS_AHORRO_PAGO, 'code' => 43, 'description' => "Fondos Ahorro Pago"],
+        ['id' => self::HORAS_EXTRAS, 'code' => 45, 'description' => "Horas Extras"],
+        ['id' => self::INGRESOS_DESCUENTOS_CONSECUTIVOS, 'code' => 48, 'description' => "Ingresos Descuentos Consecutivos"],
+        ['id' => self::NOVEDADES_CARGOS, 'code' => 103, 'description' => "Novedades Cargos"],
+        ['id' => self::NOVEDADES_SEGURIDAD_SOCIAL, 'code' => 56, 'description' => "Novedades Seguridad Social"],
+        ['id' => self::NOVEDADES_RETE_FUENTE, 'code' => 32, 'description' => "Novedades Rete Fuente"],
+        ['id' => self::OTROS_INGRESOS_DESCUENTOS, 'code' => 57, 'description' => "Otros Ingresos Descuentos"],
+        ['id' => self::PAGOS_EXTERNOS, 'code' => 130, 'description' => "Pagos Externos"],
+        ['id' => self::PAGO_LABOR, 'code' => 89, 'description' => "Pago Labor"],
+        ['id' => self::PAGO_PRODUCCION, 'code' => 59, 'description' => "Pago Producción"],
+        ['id' => self::PRESTAMOS, 'code' => 34, 'description' => "Préstamos"],
     ];
 
     public static function getCollection()
